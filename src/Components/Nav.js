90a1defe427819bc47c966204aaf28d900e1f39e
Wolfrogz <126734222+Wolfrogz@users.noreply.github.com>
2023-05-17 17:48:09 -0400

    Added child components and meta tags

---
+++ src/Components/Nav.js
@@ -0,0 +1,31 @@
+import logo from '../Images/Logo.svg'
+
+function Nav() {
+    return (
+        <nav>
+            <img src={logo}></img>
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+                <li>
+                    <a href="">Menu</a>
+                </li>
+                <li>
+                    <a href="">Reservations</a>
+                </li>
+                <li>
+                    <a href="">Order Online</a>
+                </li>
+                <li>
+                    <a href="">Login</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
