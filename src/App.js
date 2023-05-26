af0be42917e626e93d308167af962861b583425e
Wolfrogz <126734222+Wolfrogz@users.noreply.github.com>
2023-05-25 20:04:09 -0400

    Added booking form API (lint error with fetchAPI())

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import Nav from './Components/Nav';
 import Footer from './Components/Footer';
 import Homepage from './Components/Homepage';
 import BookingForm from './Components/BookingForm';
+import { useReducer } from 'react';
+
+const updateTimes = (state, action) => {
+  switch (action.type){
+    case "time":
+      return (fetchAPI(action.date));
+      default:
+        return state;
+  }
+}
+const today = new Date('May 25, 2023 16:17:30');
+const initializeTimes = fetchAPI(today);
 
 function App() {
+  const [state, dispatch] = useReducer(updateTimes, initializeTimes);
   return (
     <BrowserRouter>
       <Header></Header>
       <Nav></Nav>
       <Routes>
         <Route path='/' element={<Homepage/>}/>
-        <Route path='reservations' element={<BookingForm/>}/>
+        <Route path='reservations' element={<BookingForm availableTimes = {state} time = {state.value} dispatch= {dispatch}/>}/>
         <Route path='footer' element={<Footer/>}/>
       </Routes>
       <Footer></Footer>
--- src/Components/BookingForm.js
+++ src/Components/BookingForm.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function BookingForm() {
+function BookingForm( props ) {
     const [date, setDate] = useState("");
-    const [time, setTime] = useState("");
     const [guests, setGuests] = useState("");
+    const [time, setTime] = useState("");
     const [occasion, setOccasion] = useState("");
 
     const handleSubmit = (e) => {
@@ -13,25 +13,27 @@ function BookingForm() {
         setGuests("");
         setOccasion("");
     }
-
     return(
         <div className="container booking">
         <form onSubmit={handleSubmit}>
-            <label for="res-date">Choose date</label>
-            <input type="date" id="res-date" value={date} onChange={e => setDate(e.target.value)}/>
-            <label for="res-time">Choose time</label>
-            <select id="res-time " value={time} onChange={e => setTime(e.target.value)}>
-                <option>17:00</option>
-                <option>18:00</option>
-                <option>19:00</option>
-                <option>20:00</option>
-                <option>21:00</option>
-                <option>22:00</option>
+            <label htmlFor="res-date">Choose date</label>
+            <input required type="date" id="res-date" value={date} onChange={e => {
+                setDate(e.target.value)
+                props.dispatch({
+                type:"time",
+                date: e.target.valueAsDate
+                })
+                }}/>
+            <label htmlFor="res-time">Choose time</label>
+            <select required id="res-time " value={time} onChange={e => setTime(e.target.value)}>
+                {props.availableTimes?.map((option, index) => (
+                    <option key={index} value={option}>{option}</option>
+                ))}
             </select>
-            <label for="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={e => setGuests(e.target.value)}/>
-            <label for="occasion">Occasion</label>
-            <select id="occasion" value={occasion} onChange={e => setOccasion(e.target.value)}>
+            <label htmlFor="guests">Number of guests</label>
+            <input required type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={e => setGuests(e.target.value)}/>
+            <label htmlFor="occasion">Occasion</label>
+            <select required id="occasion" value={occasion} onChange={e => setOccasion(e.target.value)}>
                 <option>Birthday</option>
                 <option>Anniversary</option>
             </select>
